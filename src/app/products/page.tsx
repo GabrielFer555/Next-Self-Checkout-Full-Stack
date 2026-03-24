7fe9d21851ae5c0e31981edf430070f2a239989a
Gabriel Fernandes <kaiberimanos3@gmail.com>
2026-03-24 20:20:56 -0300

    configured prettier and tailwind formatter plugin

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,11 +1,8 @@
-import { Button } from "@/components/ui/button";
-
 const ProductPage = () => {
   return (
     <>
-      <div className="p-5 border bg-slate-500 rounded-xl">
+      <div className="h-screen w-screen bg-red-500">
         <h1>Products</h1>
-        <Button>Add Product</Button>
       </div>
     </>
   );
